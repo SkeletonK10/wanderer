05a3f4e13991c2129de0e170ae0f07875ab839c4
Deokgeun Hwang <66668299+SkeletonK10@users.noreply.github.com>
2026-09-14 15:41:03 +0900

    feat: 인자 받기

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    match &args[1..] {
+        [] => println!("사용법: wd <명령어>"),
+        [cmd] => println!("명령: {}", cmd),
+        [cmd, rest @ ..] => println!("명령: {}, 인자: {:?}", cmd, rest),
+    }
 }
